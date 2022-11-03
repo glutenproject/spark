896a0472aa51962d71b0bfc092c6d8ef2180060d
Jiaan Geng <beliefer@163.com>
2022-11-03 14:38:25 +0800

    [SPARK-40986][SQL] Move hash function from aggregate to new project

--- sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/InjectRuntimeFilter.scala
+++ sql/catalyst/src/main/scala/org/apache/spark/sql/catalyst/optimizer/InjectRuntimeFilter.scala
@@ -105,8 +105,10 @@ object InjectRuntimeFilter extends Rule[LogicalPlan] with PredicateHelper with J
       // i.e., the semi-join will be a shuffled join, which is not worthwhile.
       return filterApplicationSidePlan
     }
+    val project = Project(
+      aggregate.output.map(mayWrapWithHash).map(h => Alias(h, h.toString)()), aggregate)
     val filter = InSubquery(Seq(mayWrapWithHash(filterApplicationSideExp)),
-      ListQuery(aggregate, childOutputs = aggregate.output))
+      ListQuery(project, childOutputs = project.output))
     Filter(filter, filterApplicationSidePlan)
   }
 
