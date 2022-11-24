d12743dcf9ad8a2a5345814011beee1cddb14dd0
Jiaan Geng <beliefer@163.com>
2022-11-24 08:11:57 +0800

    Updae q67

--- examples/src/main/resources/tpcds_2_4.orig/q67.sql
+++ examples/src/main/resources/tpcds_2_4.orig/q67.sql
@@ -2,7 +2,7 @@
 
  select * from
      (select i_category, i_class, i_brand, i_product_name, d_year, d_qoy, d_moy, s_store_id,
-             sumsales, rank() over (partition by i_category order by sumsales desc) rk
+             sumsales, rank() over (order by sumsales desc) rk
       from
          (select i_category, i_class, i_brand, i_product_name, d_year, d_qoy, d_moy,
                  s_store_id, sum(coalesce(ss_sales_price*ss_quantity,0)) sumsales
