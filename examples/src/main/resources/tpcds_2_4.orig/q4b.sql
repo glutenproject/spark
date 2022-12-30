eb4c2efccbe319035f33eb41d17fbf093af182e0
Jiaan Geng <beliefer@163.com>
2022-12-30 11:50:21 +0800

    Update sql

--- examples/src/main/resources/tpcds_2_4.orig/q4b.sql
+++ examples/src/main/resources/tpcds_2_4.orig/q4b.sql
@@ -4,4 +4,4 @@ FROM store_sales
     JOIN store_returns
     ON store_sales.ss_item_sk = item.i_item_sk
         AND store_returns.sr_item_sk = item.i_item_sk
-WHERE item.i_color = 'pale'
+WHERE item.i_color = 'pale' AND i_current_price > 50 AND i_size = 'medium'
