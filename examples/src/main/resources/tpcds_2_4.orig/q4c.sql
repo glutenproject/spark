10d1ca7bc2052806c837814aad18928d9972e73f
Jiaan Geng <beliefer@163.com>
2022-12-31 20:13:25 +0800

    Update sql

--- examples/src/main/resources/tpcds_2_4.orig/q4c.sql
+++ examples/src/main/resources/tpcds_2_4.orig/q4c.sql
@@ -2,7 +2,6 @@ SELECT sum(ss_wholesale_cost) / 7.0 AS avg_yearly
 FROM store_sales, item
 WHERE i_item_sk = ss_item_sk
 	AND i_color = 'pale'
-	AND i_current_price > 50
 	AND ss_quantity < (
 		SELECT 0.2 * avg(sr_return_quantity)
 		FROM store_returns
