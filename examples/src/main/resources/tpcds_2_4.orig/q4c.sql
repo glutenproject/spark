bbc9a4dd288171f21289b82b051d4f19c4fb8a5b
Jiaan Geng <beliefer@163.com>
2022-12-31 19:43:34 +0800

    Update sql

--- examples/src/main/resources/tpcds_2_4.orig/q4c.sql
+++ examples/src/main/resources/tpcds_2_4.orig/q4c.sql
@@ -3,7 +3,6 @@ FROM store_sales, item
 WHERE i_item_sk = ss_item_sk
 	AND i_color = 'pale'
 	AND i_current_price > 50
-	AND i_size = 'medium'
 	AND ss_quantity < (
 		SELECT 0.2 * avg(sr_return_quantity)
 		FROM store_returns
