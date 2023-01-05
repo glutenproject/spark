8c236c7453908f524c6df24d9e1a132dca98d4d2
Jiaan Geng <beliefer@163.com>
2023-01-05 13:50:59 +0800

    Update sql

--- examples/src/main/resources/tpcds_2_4.orig/q4b.sql
+++ examples/src/main/resources/tpcds_2_4.orig/q4b.sql
@@ -1,7 +1,16 @@
-SELECT ss_quantity, ss_wholesale_cost, sr_fee, sr_return_ship_cost
-FROM store_sales
-    JOIN item
-    JOIN store_returns
-    ON store_sales.ss_item_sk = item.i_item_sk
-        AND store_returns.sr_item_sk = item.i_item_sk
-WHERE item.i_color = 'pale' AND item.i_current_price > 50 AND item.i_size = 'medium'
+SELECT ss_quantity, ss_wholesale_cost, sr_fee
+FROM (
+	SELECT ss_quantity, ss_wholesale_cost, ss_item_sk
+	FROM store_sales
+	WHERE ss_wholesale_cost BETWEEN 0 AND 50
+) store_sales2
+	JOIN item
+	JOIN (
+		SELECT sr_fee, sr_item_sk
+		FROM store_returns
+	) store_returns2
+	ON store_sales2.ss_item_sk = item.i_item_sk
+		AND store_returns2.sr_item_sk = item.i_item_sk
+WHERE item.i_color = 'pale'
+	AND item.i_current_price > 50
+	AND item.i_size = 'medium'
