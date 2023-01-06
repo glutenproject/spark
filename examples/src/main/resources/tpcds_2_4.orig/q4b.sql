b5367d6209b4ae09f6f969564100520b69a45a79
Jiaan Geng <beliefer@163.com>
2023-01-06 17:17:49 +0800

    Update sql

--- examples/src/main/resources/tpcds_2_4.orig/q4b.sql
+++ examples/src/main/resources/tpcds_2_4.orig/q4b.sql
@@ -1,13 +1,14 @@
 SELECT ss_quantity, ss_wholesale_cost, sr_fee
 FROM (
-	SELECT ss_quantity, ss_wholesale_cost, ss_item_sk
+	SELECT *
 	FROM store_sales
-	WHERE ss_wholesale_cost BETWEEN 0 AND 50
+	LIMIT 100000
 ) store_sales2
 	JOIN item
 	JOIN (
-		SELECT sr_fee, sr_item_sk
+		SELECT *
 		FROM store_returns
+		LIMIT 100000
 	) store_returns2
 	ON store_sales2.ss_item_sk = item.i_item_sk
 		AND store_returns2.sr_item_sk = item.i_item_sk
