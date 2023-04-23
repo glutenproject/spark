b97a71cdf69649a8d766d61cd95524789d69ffdf
Jiaan Geng <beliefer@163.com>
2023-04-23 11:16:38 +0800

    Update sql

--- examples/src/main/resources/tpcds_2_4.orig/q4d.sql
+++ examples/src/main/resources/tpcds_2_4.orig/q4d.sql
@@ -1,12 +1,12 @@
 SELECT *
 FROM (
-	SELECT *
+	SELECT ss_item_sk, sr_return_amt
 	FROM store_sales
 		JOIN store_returns ON ss_item_sk = sr_item_sk AND ss_ticket_number = sr_ticket_number
 	WHERE sr_return_amt > 10000
 ) a
 	JOIN (
-		SELECT *
+		SELECT ss_item_sk, c_last_name, c_first_name
 		FROM store_sales
 			JOIN customer ON ss_customer_sk = c_customer_sk
 		WHERE c_preferred_cust_flag = 'Y'
